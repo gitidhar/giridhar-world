16294751454fdf835340bad696370b2c1ec0de9a
giridhar <gpatriofficial@gmail.com>
2026-08-14 16:57:39 -0700

    description pt 6

--- app/page.tsx
+++ app/page.tsx
@@ -91,8 +91,7 @@ export default function Home() {
         }}
       >
         <p style={{ margin: 0 }}>
-          currently an engineer in fintech @ visa. Interested in stablecoin, agentic payment modalities, & looking to pursue research in various artificial 
-          intelligence paradigms.
+          currently an engineer in fintech @ visa. Interested in stablecoin, agentic payment modalities, & blockchain rails. Looking to pursue research in various artificial intelligence paradigms.
         </p>
 
         <p
